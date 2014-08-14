a08f15b716662c4e84ce5478602023fe7b1b9acf
Robin Deits <robin.deits@gmail.com>
2014-08-14 12:09:24 -0400

    working demo with mixed-integer region planning

--- QuadrupedVisualizer.m
+++ QuadrupedVisualizer.m
@@ -1,23 +1,35 @@
 classdef QuadrupedVisualizer < Visualizer
+  properties
+    safe_regions = [];
+  end
+  
    methods
-    function obj = QuadrupedVisualizer(plant)
+    function obj = QuadrupedVisualizer(plant, safe_regions)
       obj = obj@Visualizer(plant.getOutputFrame);
       obj.playback_speed = .2;
       obj.display_dt = 0;
+      obj.safe_regions = safe_regions;
     end
     
     function draw(obj,t,x)
       persistent hFig;
 
       if (isempty(hFig))
-        hFig = sfigure(25);
+        hFig = figure(25);
         set(hFig,'DoubleBuffer', 'on');
       end
       
       bot = [-0.1, -0.1, 0.1, 0.1; -0.05, 0.05, 0.05, -0.05];
       bot = bsxfun(@plus, bot, x(1:2));
       
-      sfigure(25); clf; hold on;
+      figure(25); clf; hold on;
+      
+      for j = 1:length(obj.safe_regions)        
+        V = iris.thirdParty.polytopes.lcon2vert(obj.safe_regions(j).A, obj.safe_regions(j).b);
+        V = V';
+        V = V(1:2, convhull(V(1,:), V(2,:)));
+        patch(V(1,:), V(2,:), 'k', 'FaceColor', [0.8,0.8,0.8])
+      end
       
       patch(bot(1,:), bot(2,:), 'k');
       
@@ -26,10 +38,11 @@ classdef QuadrupedVisualizer < Visualizer
       plot(x(7), x(8), 'bo', 'MarkerSize', 10)
       plot(x(9), x(10), 'mo', 'MarkerSize', 10)
       
+      
       axis equal
       xlim([-0.1; 1.1])
       ylim([-0.6; 0.6])
-      grid on
+%       grid on
     end
    end
 end

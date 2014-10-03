dfdbf4b1f6c7cf9b52e919cd0c2e4f67200058fd
Robin Deits <robin.deits@gmail.com>
2014-10-03 12:47:32 -0400

    drake revision and some uncommitted files from earlier

--- matlab/moved_to_drake/quadruped_planner.m
+++ matlab/moved_to_drake/quadruped_planner.m
@@ -1,32 +1,28 @@
-safe_regions = struct('A', {}, 'b', {}, 'point', {});
+safe_regions = struct('A', {}, 'b', {}, 'point', {}, 'normal', {});
 V = [-0.15, 0.2 .2, -0.15; 0.2, 0.2, -0.2, -0.2];
 [A, b] = poly2lincon(V(1,:), V(2,:));
-safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.3;0]);
+safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.3;0;1], 'normal', [0;0;1]);
 
 V = [0.25, 0.4 .4, 0.25; 0, 0, -0.3, -0.3];
 [A, b] = poly2lincon(V(1,:), V(2,:));
-safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.3;0]);
+safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.3;0;1], 'normal', [0;0;1]);
 
 V = [.59, 2, 2, .59; 1, 1, -1, -1];
 [A, b] = poly2lincon(V(1,:), V(2,:));
-safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.8;0]);
+safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.8;0;1], 'normal', [0;0;1]);
 
 V = [.5,.51,.5,.51; -.05, -.05, -.06, -.06];
 [A, b] = poly2lincon(V(1,:), V(2,:));
-safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.5;-.05]);
+safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.5;-.05;1], 'normal', [0;0;1]);
 
 V = [.5,.51,.51,.5; .05, .05, .06, .06];
 [A, b] = poly2lincon(V(1,:), V(2,:));
-safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.5;.05]);
+safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.5;.05;1], 'normal', [0;0;1]);
 
 V = [.45,.46,.46,.45; 0, 0, .01, .01];
 [A, b] = poly2lincon(V(1,:), V(2,:));
-safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.5;.05]);
+safe_regions(end+1) = struct('A', A, 'b', b, 'point', [.5;.05;1], 'normal', [0;0;1]);
 
-
-robot = QuadrupedPlant();
-v = QuadrupedVisualizer(robot, safe_regions);
-v.draw(0, zeros(11,1));
 
 start = struct('body', [0;0], 'rf', [0.1;-0.05],...
                               'lf', [0.1;0.05],...
@@ -37,6 +33,9 @@ goal = struct('body', [1;0]);
 gait = struct('rf', {}, 'lf', {}, 'rh', {}, 'lh', {});
 feet = fieldnames(gait)';
 
+v = SimpleGaitVisualizer(feet);
+v.safeRegions = safe_regions;
+
 gait(1) = struct('rf', true, 'lf', true, 'rh', true, 'lh', true);
 gait(2) = struct('rf', true, 'lf', true, 'rh', false, 'lh', true);
 gait(3) = struct('rf', true, 'lf', false, 'rh', false, 'lh', true);
@@ -46,9 +45,10 @@ gait(6) = struct('rf', true, 'lf', true, 'rh', true, 'lh', false);
 gait(7) = struct('rf', false, 'lf', true, 'rh', true, 'lh', false);
 gait(8) = struct('rf', false, 'lf', true, 'rh', true, 'lh', true);
 
-num_periods = 4;
+num_periods = 8;
+frames_in_gait = length(gait);
 for j = 2:num_periods
-  gait = [gait, gait];
+  gait = [gait, gait(1:frames_in_gait)];
 end
 gait(end+1) = struct('rf', true, 'lf', true, 'rh', true, 'lh', true);
 nsteps = length(gait);
@@ -134,53 +134,61 @@ for j = 1:nsteps
                               implies(rot_sector(4, j), sin_yaw(j) == -1 + cos_yaw(j))];
 end
 
+if 1
+  solvesdp(constraints, objective, sdpsettings('solver', 'gurobi'));
 
-solvesdp(constraints, objective, sdpsettings('solver', 'gurobi'));
-
-body_pos = double(body_pos);
-for f = feet
-  foot = f{1};
-  feet_pos.(foot) = double(feet_pos.(foot));
-end
-dt = double(dt);
-t = [0, cumsum(dt(1:end-1))];
-
-figure(1);
-clf
-hold on
-plot(t, body_pos(1,:), 'ko')
-% t, feet_pos.rf(1,:), t, feet_pos.lf(1,:), t, feet_pos.rh(1,:), t, feet_pos.lh(1,:))
-% legend('body', 'rf', 'lf', 'rh', 'lh')
-colors = {'r', 'g', 'b', 'm'};
-for f = 1:length(feet)
-  foot = feet{f};
-  color = colors{f};
-  for j = 1:nsteps-1
-    if gait(j).(foot)
-      plot([t(j), t(j+1)], [feet_pos.(foot)(1,j), feet_pos.(foot)(1,j+1)], '-', 'Color', color)
-    else
-      plot([t(j), t(j+1)], [feet_pos.(foot)(1,j), feet_pos.(foot)(1,j+1)], 'o', 'Color', color)
-    end
+  body_pos = double(body_pos);
+  for f = feet
+    foot = f{1};
+    feet_pos.(foot) = double(feet_pos.(foot));
   end
-end
+  dt = double(dt);
+  t = [0, cumsum(dt(1:end-1))];
 
-figure(2)
-clf
-hold on
-for j = 1:nsteps-1
-  for f = 1:length(feet)
-    foot = feet{f};
-    color = colors{f};
-    if gait(j).(foot)
-      plot([t(j), t(j+1)], [f, f], '-', 'Color', color, 'LineWidth', 5)
-    end
-  end
-end
-legend(feet{:})
+  % figure(1);
+  % clf
+  % hold on
+  % plot(t, body_pos(1,:), 'ko')
+  % % t, feet_pos.rf(1,:), t, feet_pos.lf(1,:), t, feet_pos.rh(1,:), t, feet_pos.lh(1,:))
+  % % legend('body', 'rf', 'lf', 'rh', 'lh')
+  % colors = {'r', 'g', 'b', 'm'};
+  % for f = 1:length(feet)
+  %   foot = feet{f};
+  %   color = colors{f};
+  %   for j = 1:nsteps-1
+  %     if gait(j).(foot)
+  %       plot([t(j), t(j+1)], [feet_pos.(foot)(1,j), feet_pos.(foot)(1,j+1)], '-', 'Color', color)
+  %     else
+  %       plot([t(j), t(j+1)], [feet_pos.(foot)(1,j), feet_pos.(foot)(1,j+1)], 'o', 'Color', color)
+  %     end
+  %   end
+  % end
+  % 
+  % figure(2)
+  % clf
+  % hold on
+  % for j = 1:nsteps-1
+  %   for f = 1:length(feet)
+  %     foot = feet{f};
+  %     color = colors{f};
+  %     if gait(j).(foot)
+  %       plot([t(j), t(j+1)], [f, f], '-', 'Color', color, 'LineWidth', 5)
+  %     end
+  %   end
+  % end
+  % legend(feet{:})
 
-yaw = atan2(double(sin_yaw), double(cos_yaw));
-x = [body_pos; yaw; feet_pos.(feet{1}); feet_pos.(feet{2}); feet_pos.(feet{3}); feet_pos.(feet{4})];
-xtraj = PPTrajectory(foh(t, x));
-xtraj = setOutputFrame(xtraj, robot.getStateFrame());
+  yaw = atan2(double(sin_yaw), double(cos_yaw));
+  z = ones(size(yaw));
+  x = [body_pos; z; yaw; 
+       feet_pos.(feet{1}); z; yaw;
+       feet_pos.(feet{2}); z; yaw; 
+       feet_pos.(feet{3}); z; yaw;
+       feet_pos.(feet{4}); z; yaw];
+  xtraj = PPTrajectory(foh(t, x));
+else
+  load('xtraj.mat', 'xtraj');
+end
+xtraj = setOutputFrame(xtraj, v.inputFrame);
 
 v.playback(xtraj, struct('slider', true));

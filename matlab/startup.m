6d4bd25d58ba78df71173e411ac58065afd98108
Robin Deits <robin.deits@gmail.com>
2014-10-08 15:38:56 -0400

    global makefile

--- matlab/startup.m
+++ matlab/startup.m
@@ -1,9 +1,7 @@
 path(pathdef)
 tbxmanager restorepath
-run([getenv('DRC_BASE'), '/software/build/config/drc_control_setup.m'])
 javaaddpath('/usr/local/share/java/lcm.jar');
 javaaddpath('/home/rdeits/drc/software/build/share/java/lcmtypes_drake.jar');
-javaaddpath('/home/rdeits/drc/software/build/share/java/lcmtypes_drc_lcmtypes.jar');
 
 addpath('../build/matlab');
 addpath('../drake/systems/robotInterfaces/test');
